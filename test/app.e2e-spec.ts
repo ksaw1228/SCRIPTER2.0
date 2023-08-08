75ea3c1ad423595f289af04def7aa68638a14613
woomin <ksaw1228@gmail.com>
2023-08-08 15:46:40 +0900

    e2e boards done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,6 +7,7 @@ import { typeORMTestConfig } from '../src/configs/typeormTest.config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let jwt: string;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -28,7 +29,7 @@ describe('AppController (e2e)', () => {
         .expect(401);
     });
 
-    it('/auth/signup (회원가입)', () => {
+    it('/auth/signup @Post (회원가입)', () => {
       return request(app.getHttpServer())
         .post('/auth/signup')
         .send({
@@ -38,7 +39,7 @@ describe('AppController (e2e)', () => {
         .expect(201);
     });
 
-    it('/auth/signin (회원가입후 로그인)', () => {
+    it('/auth/signin @Post (회원가입후 로그인)', () => {
       return request(app.getHttpServer())
         .post('/auth/signin')
         .send({
@@ -50,7 +51,75 @@ describe('AppController (e2e)', () => {
           const responseBody = res.body;
           expect(responseBody).toHaveProperty('accessToken'); // accessToken 속성이 있는지 확인
           expect(typeof responseBody.accessToken).toBe('string'); // accessToken의 값이 문자열인지 확인
+          jwt = responseBody.accessToken;
         });
+    });
+  });
+
+  describe('/boards (게시판)', () => {
+    it('/boards @Get(토큰이 없는 경우 401)', () => {
+      return request(app.getHttpServer()).get('/boards').expect(401);
+    });
+
+    it('/boards @Post(게시물 작성)', () => {
+      return request(app.getHttpServer())
+        .post('/boards')
+        .set('Authorization', `Bearer ${jwt}`)
+        .send({
+          title: 'test title',
+          description: 'test decriptrion',
+        })
+        .expect(201);
+    });
+
+    it('/boards @Get(모든 게시물 조회)', () => {
+      return request(app.getHttpServer())
+        .get('/boards')
+        .set('Authorization', `Bearer ${jwt}`)
+        .expect(200)
+        .expect((res) => {
+          const responseBody = res.body;
+          expect(responseBody.length).toBe(1);
+          expect(responseBody[0]).toEqual({
+            id: 1,
+            title: 'test title',
+            description: 'test decriptrion',
+            status: 'PUBLIC',
+          });
+        });
+    });
+
+    it('/boards/:id @Get(특정 게시물 조회)', () => {
+      return request(app.getHttpServer())
+        .get('/boards/1')
+        .set('Authorization', `Bearer ${jwt}`)
+        .expect(200)
+        .expect((res) => {
+          const responseBody = res.body;
+          expect(responseBody).toEqual({
+            id: 1,
+            title: 'test title',
+            description: 'test decriptrion',
+            status: 'PUBLIC',
+          });
+        });
+    });
+
+    describe('/boards/:id (DELETE 게시물)', () => {
+      it('게시물 삭제 성공', () => {
+        return request(app.getHttpServer())
+          .delete(`/boards/1`)
+          .set('Authorization', `Bearer ${jwt}`) // 인증 토큰을 포함한 요청
+          .expect(200);
+      });
+
+      it('게시물 삭제 권한 에러', () => {
+        const unauthorizedJwt = jwt + 'hacked';
+        return request(app.getHttpServer())
+          .delete(`/boards/1`)
+          .set('Authorization', `Bearer ${unauthorizedJwt}`) // 무단 사용자의 인증 토큰을 포함한 요청
+          .expect(401);
+      });
     });
   });
 });
